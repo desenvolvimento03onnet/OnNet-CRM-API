b586871e83cb53bc6889192af0817567b9cc4b73
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-05-04 08:43:47 -0300

    Correção da nomenclatura em uma das query em filter interview controller

--- app/Controllers/Http/FilterInterviewController.js
+++ app/Controllers/Http/FilterInterviewController.js
@@ -5,20 +5,20 @@ const Interview = use('App/Models/Interview');
 class FilterInterviewController {
 
     async interviewsByCity() {
-        const interview = Interview.query()
+        const interview = await Interview.query()
             .select('cities.id', 'cities.name')
             .rightJoin('cities', 'interviews.city_id', 'cities.id')
-            .count('interviews.id')
+            .count('interviews.id AS count')
             .groupBy('cities.id')
 
         return interview;
     }
 
     async interviewsByUser() {
-        const interview = Interview.query()
+        const interview = await Interview.query()
             .select('users.id', 'users.name')
             .innerJoin('users', 'interviews.user_id', 'users.id')
-            .count('interviews.id')
+            .count('interviews.id AS count')
             .groupBy('users.id')
 
         return interview;
