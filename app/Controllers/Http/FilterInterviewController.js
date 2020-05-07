aeff228097326a5c04f4345c1f09e054e746f34b
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-05-07 10:12:21 -0300

    Ajuste de parametros em Filter Interview Controller

--- app/Controllers/Http/FilterInterviewController.js
+++ app/Controllers/Http/FilterInterviewController.js
@@ -4,24 +4,28 @@ const Interview = use('App/Models/Interview');
 
 class FilterInterviewController {
 
-    async interviewsByCity() {
-        const interview = await Interview.query()
+    async interviewsByCity({ request }) {
+        const { active } = request.get();
+        const interview = Interview.query()
             .select('cities.id', 'cities.name')
             .rightJoin('cities', 'interviews.city_id', 'cities.id')
-            .count('interviews.id AS count')
-            .groupBy('cities.id')
 
-        return interview;
+        if (active)
+            interview.where('cities.active', active)
+
+        return await interview.count('interviews.id AS count').groupBy('cities.id');
     }
 
-    async interviewsByUser() {
-        const interview = await Interview.query()
+    async interviewsByUser({ request }) {
+        const { active } = request.get();
+        const interview = Interview.query()
             .select('users.id', 'users.name')
             .innerJoin('users', 'interviews.user_id', 'users.id')
-            .count('interviews.id AS count')
-            .groupBy('users.id')
 
-        return interview;
+        if (active)
+            interview.where('users.active', active)
+
+        return await interview.count('interviews.id AS count').groupBy('users.id');
     }
 
 }
