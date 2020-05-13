570c671ccb2868d11e4aa43bd458dfe16498f040
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-05-13 08:25:27 -0300

    Alteração em rota Filter Search Controller para receber parâmetros

--- app/Controllers/Http/FilterSearchQuestController.js
+++ app/Controllers/Http/FilterSearchQuestController.js
@@ -4,20 +4,28 @@ const SearchQuest = use('App/Models/SearchQuest');
 
 class FilterSearchQuestController {
 
-    async findBySearch({ params }) {
-        const searchQuest = await SearchQuest.query()
-            .where('search_id', params.id)
-            .with('search').with('quest').fetch();
+    async findBySearch({ params, request }) {
+        const { active } = request.get();
+        const searchQuest = SearchQuest.query()
+            .innerJoin('quests', 'search_quests.quest_id', 'quests.id')
+            .where('search_quests.search_id', params.id)
 
-        return searchQuest;
+        if (active)
+            searchQuest.where('quests.active', active)
+
+        return await searchQuest.with('search').with('quest').fetch();
     }
 
-    async findByQuest({ params }) {
-        const searchQuest = await SearchQuest.query()
-            .where('quest_id', params.id)
-            .with('search').with('quest').fetch();
+    async findByQuest({ params, request }) {
+        const { active } = request.get();
+        const searchQuest = SearchQuest.query()
+            .innerJoin('searches', 'search_quests.search_id', 'searches.id')
+            .where('search_quests.quest_id', params.id)
 
-        return searchQuest;
+        if (active)
+            searchQuest.where('searches.active', active)
+
+        return await searchQuest.with('search').with('quest').fetch();
     }
 
 }
--- database/migrations/1585571482596_interview_schema.js
+++ database/migrations/1585571482596_interview_schema.js
@@ -7,7 +7,6 @@ class InterviewSchema extends Schema {
     this.create('interviews', (table) => {
       table.increments()
       table.string('client_name').notNullable()
-      table.date('interview_date').notNullable()
       table.boolean('finished').notNullable().defaultTo(false)
       table.integer('search_id')
         .unsigned()
