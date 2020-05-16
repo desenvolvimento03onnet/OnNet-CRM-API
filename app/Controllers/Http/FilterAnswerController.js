5dd91813ed29260dd36835766db61a21f03dcc5c
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-05-16 10:17:55 -0300

    Atualização de Filter Answer Controller

--- app/Controllers/Http/FilterAnswerController.js
+++ app/Controllers/Http/FilterAnswerController.js
@@ -6,16 +6,6 @@ const Quest = use('App/Models/Quest');
 
 class FilterAnswerController {
 
-    async getQuests(search_id) {
-        var quest = await Quest.query()
-            .select('quests.id AS id', 'quests.question')
-            .innerJoin('search_quests', 'quests.id', 'search_quests.quest_id')
-            .where('search_quests.search_id', search_id)
-            .orderBy('search_quests.id').fetch();
-
-        return quest.rows;
-    }
-
     async getInterviews(search_id, city, begin, end) {
         const interview = Interview.ids()
             .where('search_id', search_id)
@@ -32,40 +22,23 @@ class FilterAnswerController {
         return await interview;
     }
 
-    async getAnswers(quests, interviews) {
-        var query = [];
-
-        for (var i = 0; i < quests.length; i++) {
-            const answer = await Answer.query()
-                .select('answers.rate')
-                .whereIn('answers.interview_id', interviews)
-                .where('answers.quest_id', quests[i].id)
-                .count('answers.id AS count')
-                .groupBy('answers.rate')
-                .orderBy('answers.rate')
-
-            query.push({
-                quest_id: quests[i].id,
-                question: quests[i].question,
-                rates: answer
-            })
-        }
-
-        return query;
-    }
-
-    async countRates({ params, request }) {
-        const { city, begin, end } = request.get();
+    async countRates({ params, request, response }) {
+        const { quest, city, begin, end } = request.get();
 
         const interview = await this.getInterviews(params.id, city, begin, end);
 
-        if (interview.length > 0) {
-            const quest = await this.getQuests(params.id);
+        if (!quest)
+            return response.status(500).json({error: "Não foi possível encontrar o parâmetro 'quest'"});
 
-            return await this.getAnswers(quest, interview);
-        }
+        const answer = await Answer.query()
+            .select('answers.rate')
+            .whereIn('answers.interview_id', interview)
+            .where('answers.quest_id', quest)
+            .count('answers.id AS count')
+            .groupBy('answers.rate')
+            .orderBy('answers.rate')
 
-        return [];
+        return answer;
     }
 
 }
