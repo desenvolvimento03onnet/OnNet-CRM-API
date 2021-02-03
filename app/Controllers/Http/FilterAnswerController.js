6651a65fd3641b3f97d77ef71523803f1ff40222
desenvolvimento03onnet <desenvolvimento03.onnetmais@gmail.com>
2021-02-03 12:59:28 -0300

    countRates ajustado para filtrar por setor

--- app/Controllers/Http/FilterAnswerController.js
+++ app/Controllers/Http/FilterAnswerController.js
@@ -1,59 +1,57 @@
-'use strict'
+"use strict";
 
-const Answer = use('App/Models/Answer');
-const Interview = use('App/Models/Interview');
+const Answer = use("App/Models/Answer");
+const Interview = use("App/Models/Interview");
 
 class FilterAnswerController {
+  async getInterviews(search_id, city, begin, end) {
+    const interview = Interview.ids().where("search_id", search_id);
 
-    async getInterviews(search_id, city, begin, end) {
-        const interview = Interview.ids()
-            .where('search_id', search_id)
+    if (city) interview.where("city_id", city);
 
-        if (city)
-            interview.where('city_id', city);
+    if (begin) interview.where("interview_date", ">=", begin + " 00:00:00");
 
-        if (begin)
-            interview.where('interview_date', '>=', begin + ' 00:00:00');
+    if (end) interview.where("interview_date", "<=", end + " 23:59:59");
 
-        if (end)
-            interview.where('interview_date', '<=', end + ' 23:59:59');
+    return await interview;
+  }
 
-        return await interview;
-    }
+  async countRates({ params, request, response }) {
+    const { quest, city, department, begin, end } = request.get();
 
-    async countRates({ params, request, response }) {
-        const { quest, city, begin, end } = request.get();
+    const interview = await this.getInterviews(params.id, city, begin, end);
 
-        const interview = await this.getInterviews(params.id, city, begin, end);
+    if (!quest)
+      return response
+        .status(500)
+        .json({ error: "Não foi possível encontrar o parâmetro 'quest'" });
 
-        if (!quest)
-            return response.status(500).json({ error: "Não foi possível encontrar o parâmetro 'quest'" });
+    const answer = Answer.query()
+      .select("answers.rate")
+      .whereIn("answers.interview_id", interview)
+      .where("answers.quest_id", quest);
 
-        const answer = await Answer.query()
-            .select('answers.rate')
-            .whereIn('answers.interview_id', interview)
-            .where('answers.quest_id', quest)
-            .count('answers.id AS count')
-            .groupBy('answers.rate')
-            .orderBy('answers.rate')
+    if (department) answer.where("answers.department_id", department);
 
-        return answer;
-    }
+    return await answer
+      .count("answers.id AS count")
+      .groupBy("answers.rate")
+      .orderBy("answers.rate");
+  }
 
-    async getNotesByQuest({ params, request }) {
-        const page = request.get().page || 1;
+  async getNotesByQuest({ params, request }) {
+    const page = request.get().page || 1;
 
-        const answer = Answer.query()
-            .innerJoin('interviews', 'answers.interview_id', 'interviews.id')
-            .where('answers.quest_id', params.id)
-            .where('answers.note', 'IS NOT', null)
-            .with('interview')
-            .orderBy('interviews.interview_date', 'DESC')
-            .paginate(page, 50);
+    const answer = Answer.query()
+      .innerJoin("interviews", "answers.interview_id", "interviews.id")
+      .where("answers.quest_id", params.id)
+      .where("answers.note", "IS NOT", null)
+      .with("interview")
+      .orderBy("interviews.interview_date", "DESC")
+      .paginate(page, 50);
 
-        return await answer;
-    }
-
+    return await answer;
+  }
 }
 
-module.exports = FilterAnswerController
+module.exports = FilterAnswerController;
